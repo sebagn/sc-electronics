59ddcb21750b94871b719478781404ed548088d5
Sebastian Garcia Nallar <sebagn@hotmail.com>
2021-11-24 14:33:33 -0300

    faltaron subir 2 archivos para la 1er entrega

---
+++ src/components/CartView/CartView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartView = () => {
+    return (
+        <div>
+            <h1>Cart View</h1>      
+        </div>
+    )
+}
+
+export default CartView
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './NotFound.scss'
+import { Link } from 'react-router-dom'
+
+const NotFound = () => {
+    return (
+        <div class="info">
+            <h1> 404 </h1>
+            <h2>
+                No encontramos la pagina que buscas
+            </h2>
+            <p>
+                Lo siento, intenta volver a ver los mejores productos en SC-Electronics
+            </p>
+            <Link to="/" className="btn btn-success">Volver a SC-Electronics</Link>
+        </div>
+    )
+}
+
+export default NotFound
